107abd8ac4b9ed3c9d4789167fbd23d5d0ea23e6
Sophie Tauchert <999eagle@999eagle.moe>
2020-01-31 21:45:27 +0100

    Add basic player with movement

---
+++ Assets/Script/Planet/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script.Planet
+{
+	public class Player : MonoBehaviour
+	{
+		private GameObject playerSprite;
+
+		// Start is called before the first frame update
+		private void Start()
+		{
+			playerSprite = transform.Find("Robot").gameObject;
+		}
+
+		// Update is called once per frame
+		private void Update()
+		{
+			var x = Input.GetAxis("Horizontal");
+			var y = Input.GetAxis("Vertical");
+			var move = new Vector3(x, y, 0);
+			move.Normalize();
+			transform.Translate(5 * Time.deltaTime * move);
+			if (move.sqrMagnitude > 0)
+			{
+				playerSprite.transform.rotation =
+					Quaternion.AngleAxis(Vector3.SignedAngle(Vector3.right, move, Vector3.forward), Vector3.forward);
+			}
+		}
+	}
+}
