1c65d80791f63c673a6d8fd1187533ec21a7df30
Sophie Tauchert <999eagle@999eagle.moe>
2020-01-31 23:22:29 +0100

    Fix lifebar scaling

--- Assets/Script/UI/Lifebar.cs
+++ Assets/Script/UI/Lifebar.cs
@@ -12,17 +12,25 @@ namespace Script.UI
 		private float value = 1f;
 		private GameObject bar;
 		private SpriteRenderer barSpriteRenderer;
+		private GameObject shadow;
+		private SpriteRenderer shadowSpriteRenderer;
 
 		private void Start()
 		{
 			bar = transform.Find("Bar").gameObject;
 			barSpriteRenderer = bar.GetComponent<SpriteRenderer>();
+			shadow = transform.Find("Shadow").gameObject;
+			shadowSpriteRenderer = shadow.GetComponent<SpriteRenderer>();
 		}
 
 		private void Update()
 		{
-			barSpriteRenderer.size = new Vector2(value, 0.26f);
-			bar.transform.localPosition = new Vector3(-0.5f + value / 2f, 0f, 0f);
+			var scale = transform.lossyScale;
+			bar.transform.localScale = new Vector3(1f / scale.x, 1f / scale.y, 1f);
+			barSpriteRenderer.size = new Vector2(value * scale.x, 0.26f * scale.y);
+			shadow.transform.localScale = bar.transform.localScale;
+			shadowSpriteRenderer.size = new Vector2(scale.x, 0.26f * scale.y);
+			bar.transform.localPosition = new Vector3((value - 1) * 0.5f, 0f, 0f);
 			if (value > 0.5f)
 			{
 				barSpriteRenderer.sprite = greenBar;
